8514292bdfc53599c8f8071da0fec1463d186919
devtiberio <tiberiope@gmail.com>
2022-03-22 17:09:43 -0300

    Criando arquivo numero.py

---
+++ numeros.py
@@ -0,0 +1,19 @@
+soma = 5 + 4
+subtracao = 12 - 3
+multiplicacao = 3 * 3
+divisaoSemResto = 27 / 3
+divisaoResto = 109 % 10
+divisaoInteiro = 29 // 3
+exponencial = 3 ** 2
+
+print("Olá, eu tenho " + str(33) + ", e você?")
+idade = input("Digite a sua idade: ")
+print("Eu tenho: " + idade + '\n')
+
+print(soma)
+print(subtracao)
+print(multiplicacao)
+print(divisaoSemResto)
+print(divisaoResto)
+print(divisaoInteiro)
+print(exponencial)
