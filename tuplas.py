140f3dc7546cb112dd682dd4217234b2279734e3
devtiberio <tiberiope@gmail.com>
2022-03-22 19:31:40 -0300

    Criando o arquivo tuplas.py

---
+++ tuplas.py
@@ -0,0 +1,9 @@
+#criando uma tupla: elemento de tamanho fixo, ou seja, é um elemento constante (não-variável).
+alimentos = ('cachorro-quente', 'hamburguer', 'batata frita')
+print(alimentos)
+print(alimentos[0])
+print("----------")
+
+#percorrendo uma tupla.
+for alimento in alimentos:
+    print(alimento)
