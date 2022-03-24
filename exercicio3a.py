e5ee1302a41c1c3150e5058b303936f6f2071ea7
devtiberio <tiberiope@gmail.com>
2022-03-24 13:47:33 -0300

    Resposta dos exercícios

--- exercicio3a.py
+++ exercicio3a.py
@@ -7,5 +7,8 @@ def sub(a, b):
 def div (a, b):
     return a/b
 
+def div_resto (a, b):
+    return a%b
+
 def mult (a, b):
     return a*b
--- exercicio4.py
+++ exercicio4.py
@@ -1,17 +1,17 @@
-from exercicio3a import soma
+from exercicio3a import soma, div_resto
 escolha = input("Escolha P para par ou I para impar: ")
 a = int(input("Escolha o primeiro valor: "))
 b = int(input("Escolha o segundo valor: "))
 
 
 if (escolha.upper() == 'I'):
-    if (soma(a,b) % 2 == 1):
+    if div_resto(soma(a,b), 2) == 1:
         print('Deu IMPAR... Parabéns, você venceu!')
     else:
         print('Deu PAR... Você perdeu!')
 
 elif (escolha.upper() == 'P'):
-    if (soma(a,b) % 2 == 0):
+    if div_resto(soma(a,b), 2) == 0:
         print('Deu PAR... Parabéns, você venceu!')
     else:
         print('Deu IMPAR... Você perdeu!')
