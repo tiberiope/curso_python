1ca2eb8bd4eb6d718a36183ff97d355fee46e291
devtiberio <tiberiope@gmail.com>
2022-03-24 12:29:09 -0300

    Criando os arquivos modulo.py e programa_funcoes.py

---
+++ modulo.py
@@ -0,0 +1,7 @@
+def lista_funcao(lista):
+    for cor in lista:
+        print(cor)
+
+def soma(a,b):
+    return a + b
+
---
+++ programa_funcoes.py
@@ -0,0 +1,8 @@
+import modulo
+#ou from modulo import *
+
+lista_cor = ['Vermelho', 'Verde', 'Preto', 'Branco', 'Azul']
+modulo.lista_funcao(lista_cor)
+
+print(modulo.soma(5,10))
+
