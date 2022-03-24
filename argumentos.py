3b3372892a5e4dece922f492f6c72503c41b21dc
devtiberio <tiberiope@gmail.com>
2022-03-24 11:42:05 -0300

    Criando arquivo argumentos.py

--- argumentos.py
+++ argumentos.py
@@ -15,10 +15,18 @@ def nome_completo(primeiro_nome='', ultimo_nome='', nome_do_meio=''):
 
     if nome_do_meio != '':
         print('O nome digitado foi: ' + primeiro_nome.title() + ' ' + nome_do_meio.title() + ' ' + ultimo_nome.title())
+        ret_nome_completo = primeiro_nome.title() + ' ' + nome_do_meio.title() + ' ' + ultimo_nome.title()
     else:
         print('O nome digitado foi: ' + primeiro_nome.title() + ' ' + ultimo_nome.title())
+        ret_nome_completo = primeiro_nome.title() + ' ' + ultimo_nome.title()
 
+    return ret_nome_completo
+
+
+ret_nome_completo = nome_completo(ultimo_nome='Pessoa', primeiro_nome='artur')
 nome_completo(ultimo_nome='Pessoa', primeiro_nome='artur', nome_do_meio='tibério')
-nome_completo(ultimo_nome='Pessoa', primeiro_nome='artur')
 nome_completo('primeiro', 'ultimo', 'meio')
+
+
+print(ret_nome_completo)
 
