c1606b18136d379c92aac2878e5f59e2b7732d15
devtiberio <tiberiope@gmail.com>
2022-03-28 19:37:55 -0300

    Corrigindo bug do executável

--- exercicios/exercicio4.py
+++ exercicios/exercicio4.py
@@ -1,17 +1,21 @@
-from curso_python.exercicios.exercicio3a import soma, div_resto
-escolha = input("Escolha P para par ou I para impar: ")
-a = int(input("Escolha o primeiro valor: "))
-b = int(input("Escolha o segundo valor: "))
+import exercicio3a
 
+continuar = 's'
+while continuar.lower() == 's':
+    escolha = input("Escolha P para par ou I para impar: ")
+    a = int(input("Escolha o primeiro valor: "))
+    b = int(input("Escolha o segundo valor: "))
 
-if (escolha.upper() == 'I'):
-    if div_resto(soma(a,b), 2) == 1:
-        print('Deu IMPAR... Parabéns, você venceu!')
-    else:
-        print('Deu PAR... Você perdeu!')
+    if (escolha.upper() == 'I'):
+        if exercicio3a.div_resto(exercicio3a.soma(a,b), 2) == 1:
+            print('Deu IMPAR... Parabéns, você venceu!')
+        else:
+            print('Deu PAR... Você perdeu!')
 
-elif (escolha.upper() == 'P'):
-    if div_resto(soma(a,b), 2) == 0:
-        print('Deu PAR... Parabéns, você venceu!')
-    else:
-        print('Deu IMPAR... Você perdeu!')
+    elif (escolha.upper() == 'P'):
+        if exercicio3a.div_resto(exercicio3a.soma(a,b), 2) == 0:
+            print('Deu PAR... Parabéns, você venceu!')
+        else:
+            print('Deu IMPAR... Você perdeu!')
+
+    continuar = input('Deseja jogar novamente? s para sim: ')
