8519729b922ab4acae7f2bee3bdec1ba95f14b3a
Tibério Pessoa <101218442+tiberiope@users.noreply.github.com>
2022-03-28 18:25:34 -0300

    Add files via upload

---
+++ armazenamento_dados.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+conexao = sqlite3.connect('aula44.db')
+c = conexao.cursor()
+
+#c.execute('CREATE TABLE IF NOT EXISTS usuario(id integer, nome text)')
+
+#c.execute("INSERT INTO usuario VALUES (1, 'Artur')")
+#c.execute("INSERT INTO usuario VALUES (2, 'Rodrigo')")
+#c.execute("INSERT INTO usuario VALUES (3, 'Pedro')")
+
+#conexao.commit()
+
+requisicao = 'SELECT * FROM usuario WHERE nome = ?'
+
+for linha in c.execute(requisicao, ('Artur',)):
+    print(linha)
